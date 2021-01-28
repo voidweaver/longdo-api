3f22185c37e06c39a7d4b62f3a8b2812f6b64008
voidweaver <44946758+voidweaver@users.noreply.github.com>
2021-01-28 21:00:14 +0700

    (m) Format code

--- src/helper.ts
+++ src/helper.ts
@@ -1,14 +1,8 @@
 // Probably inefficient, but that's the best I can come up with
 export function clean_2d_array(array: string[][], span: number): string[][] {
     for (let i = 0; i < span; i++) {
-        if (
-            array.every((row) => {
-                return row[i].trim().length === 0;
-            })
-        ) {
-            array.forEach((row) => {
-                row.splice(i, 1);
-            });
+        if (array.every((row) => row[i].trim().length === 0)) {
+            array.forEach((row) => row.splice(i, 1));
             i--;
             span--;
         }
